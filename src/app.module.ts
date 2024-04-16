3335fc3b6858573547b347952582ace53e27937c
GabrielFrahm <gabriel_frahm@hotmail.com>
2024-04-16 07:45:59 -0300

    feat(): separete rules of business

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from '@src/prisma.service';
+
+import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { ContentController } from '@src/http/rest/controller/content.controller';
+import { MediaPlayerService } from './core/service/media-player.service';
+import { ContentManagementService } from './core/service/content-management.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [ContentController],
+  providers: [PrismaService, MediaPlayerService, ContentManagementService],
 })
 export class AppModule {}
