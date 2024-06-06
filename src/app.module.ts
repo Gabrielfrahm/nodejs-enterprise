8c5ba3e904fbbbaefdd07921b51b4bf9d6b288e9
GabrielFrahm <gabriel_frahm@hotmail.com>
2024-06-06 15:15:03 -0300

    fear(): migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,10 @@ import { ContentManagementService } from './core/service/content-management.serv
 import { ContentRepository } from './persistence/repository/content.repository';
 import { VideoRepository } from './persistence/repository/video.repository';
 import { MediaPlayerController } from './http/rest/controller/media-player.controller';
-import { ConfigModule } from './infra/module/config/config.module';
+import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [PersistenceModule.forRoot()],
   controllers: [ContentController, MediaPlayerController],
   providers: [
     MediaPlayerService,
